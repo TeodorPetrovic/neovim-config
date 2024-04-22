1be1e50093a043097e57d7d81216c799665f6a64
TeodorPetrovic <teodor.z.petrovic@gmail.com>
2024-04-22 11:49:12 +0200

    Fixed make and tree-sitter

--- lua/masofino/plugins/nvim-cmp.lua
+++ lua/masofino/plugins/nvim-cmp.lua
@@ -8,8 +8,6 @@ return {
       "L3MON4D3/LuaSnip",
       -- follow latest release.
       version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-      -- install jsregexp (optional!).
-      build = "mingw32-make install_jsregexp", -- make for windows
     },
     "saadparwaiz1/cmp_luasnip", -- for autocompletion
     "rafamadriz/friendly-snippets", -- useful snippets

2546da45b25b8fb65f38520a5e58e485c5dfb069
Teodor Petrovic <teodor.z.petrovic@gmail.com>
2024-08-15 07:01:07 +0200

    Updated formating to 5000 ms

--- lua/masofino/plugins/formatting.lua
+++ lua/masofino/plugins/formatting.lua
@@ -32,8 +32,8 @@ return {
 		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
 			conform.format({
 				lsp_fallback = true,
-				async = true,
-				timeout_ms = 3000,
+				async = false,
+				timeout_ms = 5000,
 			})
 		end, { desc = "Format file or range (in visual mode)" })
 	end,

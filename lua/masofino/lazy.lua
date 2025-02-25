d194f82418b265dac70d0f46ac7c0ec39526e2ef
Teodor Petrovic <teodor.z.petrovic@gmail.com>
2025-02-25 15:38:27 +0100

    Some fix

--- lua/masofino/lazy.lua
+++ lua/masofino/lazy.lua
@@ -1,22 +1,36 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({{ import = "masofino.plugins" }, { import = "masofino.plugins.lsp"} }, {
-    checker = {
-        enabled = true,
-        notify = false,
-    },
-    change_detection = {
-        notify = false,
-    },
+require("lazy").setup({ { import = "masofino.plugins" }, { import = "masofino.plugins.lsp" } }, {
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
 })
+
+vim.opt.clipboard:append("unnamedplus")
+vim.g.clipboard = {
+	name = "xsel",
+	copy = {
+		["+"] = "xsel --clipboard --input",
+		["*"] = "xsel --input",
+	},
+	paste = {
+		["+"] = "xsel --clipboard --output",
+		["*"] = "xsel --output",
+	},
+	cache_enabled = 1,
+}

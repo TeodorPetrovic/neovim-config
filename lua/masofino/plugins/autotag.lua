601c65b63bfd4ae5e56fb41dded4ea41f3863cd4
Teodor Petrovic <teodor.z.petrovic@gmail.com>
2025-12-18 06:22:55 +0000

    fix: A lot of fixes

--- lua/masofino/plugins/autotag.lua
+++ lua/masofino/plugins/autotag.lua
@@ -2,11 +2,9 @@ return {
 	"windwp/nvim-ts-autotag",
 	config = function()
 		require("nvim-ts-autotag").setup({
-			opts = {
-				enable_close = true, -- Auto close tags
-				enable_rename = true, -- Auto rename pairs of tags
-				enable_close_on_slash = true, -- Auto close on trailing </
-			},
+			enable_close = true,
+			enable_rename = true,
+			enable_close_on_slash = true,
 		})
 	end,
 }
--- lua/masofino/plugins/rest.lua
+++ lua/masofino/plugins/rest.lua
@@ -7,4 +7,30 @@ return {
 			table.insert(opts.ensure_installed, "http")
 		end,
 	},
+	config = function()
+		-- Set formatprg for JSON using jq
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "json",
+			callback = function()
+				vim.opt_local.formatprg = "jq"
+			end,
+		})
+
+		-- Set formatprg for HTML using tidy
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "html",
+			callback = function()
+				vim.opt_local.formatprg = "tidy -i -q"
+			end,
+		})
+
+		vim.g.rest_nvim = {
+			result = {
+				formatters = {
+					json = "jq",
+					html = "tidy",
+				},
+			},
+		}
+	end,
 }
